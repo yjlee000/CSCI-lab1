2cc44eaa55682e73736c1e98926dd8a08a3eeacc
Yejin Lee <128701398+yjlee000@users.noreply.github.com>
2023-09-12 19:54:31 +0900

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,17 @@
+# you can add imports but you should not rely on libraries that are not already provided in "requirements.txt #
+from collections import deque
+
+def breadth_first_search(stack):
+    flip_sequence = []
+
+    # --- v ADD YOUR CODE HERE v --- #
+    return flip_sequence
+    # ---------------------------- #
+
+
+def depth_first_search(stack):
+    flip_sequence = []
+
+    # --- v ADD YOUR CODE HERE v --- #
+    return flip_sequence
+    # ---------------------------- #
